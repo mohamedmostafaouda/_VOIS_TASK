cac87d9dce27dbde56969c99c1647fdc4b88aeb6
Mohamed Mostafa <mmostafaouda@gmail.com>
2022-08-22 14:28:47 +0200

    Fix linter and prettier

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import styles from '../src/app.module.scss';
 
 function App() {
+  const x = 12;
   return <div className={styles.name}>{'react setup from scratch without cra testing ts'}</div>;
 }
 export default App;
