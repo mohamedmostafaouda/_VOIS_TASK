eca012c76b49278a812996da73aae09e7c529d68
Mohamed Mostafa <mmostafaouda@gmail.com>
2022-08-25 16:27:45 +0200

    fix lint

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import './i18n';
 import 'styles/styles.scss';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <Provider store={store}>
